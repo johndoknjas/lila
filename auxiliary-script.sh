dbd6c3914430213dc22c3d29db2da4f48d52b0ff
johndoknjas <jdoknjas@sfu.ca>
2026-08-30 19:04:28 -0700

    Get challenges working.

--- auxiliary-script.sh
+++ auxiliary-script.sh
@@ -20,6 +20,6 @@ tmux split-window -h "cd '$LILA_DIR' && ui/build -w; bash"
 tmux split-window -v "cd '$LILA_DIR' && killall redis-server && sleep 1; redis-server; bash"
 tmux select-pane -t 0
 tmux split-window -v "cd '$LILA_DIR' && mongod; bash"
-tmux split-window -h "cd '$LILA_WS_DIR' && source ~/.zshrc && sbt run -Dcsrf.origin=$CSRF_ORIGIN; bash"
+tmux split-window -h "cd '$LILA_WS_DIR' && source ~/.zshrc && sbt 'set javaOptions += \"-Dcsrf.origin=$CSRF_ORIGIN\"; run'; bash"
 tmux swap-pane -s 1 -t 2
 tmux -2 attach-session -d
